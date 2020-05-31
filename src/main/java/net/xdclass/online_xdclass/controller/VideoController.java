2e68c8304555049201635e15be3d081eec770350
lishuyu01 <lishuyu01@qiyi.com>
2020-05-31 23:35:02 +0800

    1. 更改接口路径 2. 统一返回数据结构JsonData

--- src/main/java/net/xdclass/online_xdclass/controller/VideoController.java
+++ src/main/java/net/xdclass/online_xdclass/controller/VideoController.java
@@ -2,6 +2,7 @@ package net.xdclass.online_xdclass.controller;
 
 import net.xdclass.online_xdclass.domain.Video;
 import net.xdclass.online_xdclass.service.VideoService;
+import net.xdclass.online_xdclass.utils.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("video")
+@RequestMapping("api/v1/pub/video")
 public class VideoController {
 
     @Autowired
     private VideoService videoService;
 
     @RequestMapping("list")
-    public List<Video> listVideo() {
-        return videoService.listVideo();
+    public JsonData listVideo() {
+        List<Video> videoList = videoService.listVideo();
+        return JsonData.buildSuccess(videoList);
     }
 }
