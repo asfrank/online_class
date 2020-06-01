09d0a0ba5f5dac7485b4ddb88689ef783e9775f0
lishuyu01 <lishuyu01@qiyi.com>
2020-06-01 20:54:15 +0800

    1. 开发JWT工具类

---
+++ src/main/java/net/xdclass/online_xdclass/utils/JWTUtils.java
@@ -0,0 +1,55 @@
+package net.xdclass.online_xdclass.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import net.xdclass.online_xdclass.domain.User;
+
+import java.util.Date;
+
+/**
+ * JWT工具类
+ */
+public class JWTUtils {
+
+    private static final long EXPIRE = 60000 * 60 * 24 * 7; // 过期时间，一周
+
+    private static final String SECRET = "xdclass.net168"; // 加密秘钥
+
+    private static final String TOKEN_PREFIX = "xdclass"; // 令牌前缀
+
+    private static final String SUBJECT = "xdclass";
+
+    /**
+     * 根据用户信息生成令牌
+     * @param user
+     * @return
+     */
+    public static String genJsonWebToken(User user) {
+        String token = Jwts.builder().setSubject(SUBJECT)
+                .claim("head_img", user.getHeadImg())
+                .claim("id", user.getId())
+                .claim("name", user.getName())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRE))
+                .signWith(SignatureAlgorithm.HS256, SECRET).compact();
+
+        token = TOKEN_PREFIX + token;
+        return token;
+    }
+
+    /**
+     * 校验token方法
+     * @param token
+     * @return
+     */
+    public static Claims checkJWT(String token) {
+        try {
+            final Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token.replace(TOKEN_PREFIX, "")).getBody();
+            return claims;
+        }catch (Exception e) {
+            return null;
+        }
+
+    }
+}
