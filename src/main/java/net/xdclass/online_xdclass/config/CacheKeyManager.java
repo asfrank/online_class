3ba781dbe76d22f538c49b9ba9ee59ade3e042de
lishuyu01 <lishuyu01@qiyi.com>
2020-06-02 23:45:11 +0800

    1. 轮播图接⼝引⼊本地缓存

---
+++ src/main/java/net/xdclass/online_xdclass/config/CacheKeyManager.java
@@ -0,0 +1,7 @@
+package net.xdclass.online_xdclass.config;
+
+public class CacheKeyManager {
+
+    // 首页轮播图缓存key
+    public static final String INDEX_BANNER_KEY = "index:banner";
+}
--- src/main/java/net/xdclass/online_xdclass/service/impl/VideoServiceImpl.java
+++ src/main/java/net/xdclass/online_xdclass/service/impl/VideoServiceImpl.java
@@ -1,19 +1,25 @@
 package net.xdclass.online_xdclass.service.impl;
 
+import net.xdclass.online_xdclass.config.CacheKeyManager;
 import net.xdclass.online_xdclass.model.entity.Video;
 import net.xdclass.online_xdclass.model.entity.VideoBanner;
 import net.xdclass.online_xdclass.mapper.VideoMapper;
 import net.xdclass.online_xdclass.service.VideoService;
+import net.xdclass.online_xdclass.utils.BaseCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class VideoServiceImpl implements VideoService {
 
     @Autowired
     private VideoMapper videoMapper;
+
+    @Autowired
+    private BaseCache baseCache;
 
     @Override
     public List<Video> listVideo() {
@@ -22,7 +28,22 @@ public class VideoServiceImpl implements VideoService {
 
     @Override
     public List<VideoBanner> listBanner() {
-        return videoMapper.listVideoBanner();
+        try {
+            Object cacheObj = baseCache.getTenMinuteCache().get(CacheKeyManager.INDEX_BANNER_KEY, ()->{
+                List<VideoBanner> bannerList = videoMapper.listVideoBanner();
+                System.out.println("从数据库读取轮播图");
+                return bannerList;
+            });
+
+            if (cacheObj instanceof List) {
+                List<VideoBanner> bannerList = (List<VideoBanner>) cacheObj;
+                return bannerList;
+            }
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
